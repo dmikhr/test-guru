a3b1d1f06fc06465f871779b4fd0b6350f9722ea
dmikhr <dmitrypkh@gmail.com>
2019-05-16 14:00:15 +0300

    Removed empty tests, rename methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # возвращает список всех Тестов, которые проходит или когда-либо
   # проходил Пользователь на этом уровне сложности
-  def taken_tests_by_level(level)
+  def passed_tests_by_level(level)
     # список test_id которые проходил пользователь
     test_ids = PassedTest.where(user_id: self.id).pluck(:test_id)
     # получаем тесты по списку test_id
